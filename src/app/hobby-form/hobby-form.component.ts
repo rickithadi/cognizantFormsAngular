70ee052a997e07e54cab488d071572e2b7ebf7cd
hadi <rickithadi@gmail.com>
2021-04-09 23:59:38 +0800

    added input and output eventemitter in hobby-form

--- src/app/hobby-form/hobby-form.component.ts
+++ src/app/hobby-form/hobby-form.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import {Hobby} from '../models/hobby'
+
+import { FormArray, FormGroup } from '@angular/forms';
+import { RxFormBuilder } from '@rxweb/reactive-form-validators';
 
 @Component({
   selector: 'app-hobby-form',
@@ -6,10 +10,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./hobby-form.component.css']
 })
 export class HobbyFormComponent implements OnInit {
+  @Input() userForm: FormGroup;
+  @Output() updateHobbies = new EventEmitter();
+  get hobbies() {
+    return this.userForm.get('hobbies') as FormArray;
+  }
 
-  constructor() { }
+
+  constructor(
+    private formBuilder: RxFormBuilder,
+  ) { }
 
   ngOnInit(): void {
+  }
+  addHobby() {
+    this.hobbies.push(this.formBuilder.formGroup(Hobby));
+
+  }
+
+  removeHobby(index: number) {
+    this.hobbies.removeAt(index);
+  }
+
+  submitForm() {
+    console.log(this.userForm.value);
+    this.updateHobbies.emit();
   }
 
 }
