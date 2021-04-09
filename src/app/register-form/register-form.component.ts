3e60106350e88ff96f2776e06288db2ecb8b725b
hadi <rickithadi@gmail.com>
2021-04-09 15:53:25 +0800

    generating class from json user

--- src/app/register-form/register-form.component.ts
+++ src/app/register-form/register-form.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { plainToClass } from 'class-transformer';
+
+
+import { RxFormBuilder, RxFormGroup } from "@rxweb/reactive-form-validators";""
+import {userJson} from '../../assets/user-data'
+import {User} from '../models/user'
 
 @Component({
   selector: 'app-register-form',
@@ -6,8 +12,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register-form.component.css']
 })
 export class RegisterFormComponent implements OnInit {
+      user = plainToClass(User, userJson);
 
-  constructor() { }
+  userForm:RxFormGroup
+
+
+  constructor(
+    private formBuilder: RxFormBuilder) { 
+
+  this.userForm = this.formBuilder.formGroup(this.user) as RxFormGroup;
+      
+    }
 
   ngOnInit(): void {
   }
